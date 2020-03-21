71030c4f7bdb7c7674ff4e95a0f003aaf92bed6f
kadouzhyu <kadouzhyu@163.com>
2020-03-21 16:47:47 +0800

    Initial commit

---
+++ src/test/java/moon/ya/yueyadao/YueyadaoApplicationTests.java
@@ -0,0 +1,13 @@
+package moon.ya.yueyadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YueyadaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
